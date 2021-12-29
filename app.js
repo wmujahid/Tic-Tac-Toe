7e625ab3b80a60c0fcdf2d77e79c9bb5d76dae25
wmujahid <wmujahid47@gmail.com>
2021-12-29 13:14:14 -0500

    playerWins function added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // I want to select everything with the class box and save it to a variable
 const boxes = document.querySelectorAll('.box');
 
+const gameText = document.getElementById('gameText');
+
 // this array will keep track of what boxes are available for a user to select
-let boardSpaces = ['', '', '', '', '', '', '', '', '']
+const boardSpaces = ['', '', '', '', '', '', '', '', '']
 
 // player 1 will be 'O' and player 2 will be 'X'
 const playerOne_O = "O";
@@ -37,6 +39,11 @@ const boxClicked = (event) => {
   if (boardSpaces[id] === '') {
     boardSpaces[id] = currentPlayer;
     event.target.innerHTML = currentPlayer;
+
+    if (playerWins()) {
+      gameText.innerText = `${currentPlayer} has won`;
+      return;
+    }
     // after a box is clicked if the current player is X switch it to O and vice versa
     if (currentPlayer === playerOne_O) {
       currentPlayer = playerTwo_X
@@ -44,6 +51,48 @@ const boxClicked = (event) => {
       currentPlayer = playerOne_O
     }
   }
-}
+};
+
+const playerWins = () => {
+  if (boardSpaces[0] === currentPlayer) {
+    if (boardSpaces[1] === currentPlayer && boardSpaces[2] === currentPlayer) {
+      console.log(`${currentPlayer} wins up top`);
+      return true;
+    }
+    if (boardSpaces[3] === currentPlayer && boardSpaces[6] === currentPlayer) {
+      console.log(`${currentPlayer} wins on the left`);
+      return true;
+    }
+    if (boardSpaces[4] === currentPlayer && boardSpaces[8] === currentPlayer) {
+      console.log(`${currentPlayer} wins on the diagonal`);
+      return true;
+    }
+  }
+  //from bottom check up and across
+  if (boardSpaces[8] === currentPlayer) {
+    if (boardSpaces[2] === currentPlayer && boardSpaces[5] === currentPlayer) {
+      console.log(`${currentPlayer} wins on the right`);
+      return true;
+    }
+    if (boardSpaces[7] === currentPlayer && boardSpaces[6] === currentPlayer) {
+      console.log(`${currentPlayer} wins on the bottom`);
+      return true;
+    }
+  }
+  //from middle check middle vertical and middle horizontal
+  if (boardSpaces[4] === currentPlayer) {
+    if (boardSpaces[3] === currentPlayer && boardSpaces[5] === currentPlayer) {
+      console.log(`${currentPlayer} wins on the middle horizontal`);
+      return true;
+    }
+    if (boardSpaces[1] === currentPlayer && boardSpaces[7] === currentPlayer) {
+      console.log(`${currentPlayer} wins on the middle vertical`);
+      return true;
+    }
+  }
+};
+
+
+
 
 createBoardLines();
